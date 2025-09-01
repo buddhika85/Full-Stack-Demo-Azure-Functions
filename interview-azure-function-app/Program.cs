79ec26038486692401039cbbac0405391013e684
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-09-01 12:31:55 +1000

    Azure Function - push to azure service bus

--- interview-azure-function-app/Program.cs
+++ interview-azure-function-app/Program.cs
@@ -1,7 +1,9 @@
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -10,5 +12,24 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+
+builder.Services.AddLogging(logging =>
+{
+    logging.AddConsole();
+});
+
+builder.Services.AddSingleton<ServiceBusClient>(provider =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("ServiceBusConnection");      // this is coming from Azure Function App Enviroment Variables
+    return new ServiceBusClient(connectionString);
+});
+
+builder.Services.AddSingleton<ServiceBusSender>(provider =>
+{
+    var client = provider.GetRequiredService<ServiceBusClient>();
+    return client.CreateSender("oddeven");
+});
+
 
 builder.Build().Run();
--- interview-azure-function-app/PushToAzureServiceBus.cs
+++ interview-azure-function-app/PushToAzureServiceBus.cs
@@ -1,3 +1,4 @@
+using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -5,19 +6,46 @@ using Microsoft.Extensions.Logging;
 
 namespace interview_azure_function_app;
 
+/// <summary>
+/// Publisher to Azure Service Bus
+/// </summary>
 public class PushToAzureServiceBus
 {
     private readonly ILogger<PushToAzureServiceBus> _logger;
+    private readonly ServiceBusSender _sender;
 
-    public PushToAzureServiceBus(ILogger<PushToAzureServiceBus> logger)
+    public PushToAzureServiceBus(ILogger<PushToAzureServiceBus> logger, ServiceBusSender sender)
     {
         _logger = logger;
+        _sender = sender;
     }
 
     [Function("PushToAzureServiceBus")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        _logger.LogInformation("Processing request...");
+
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        using var messageBatch = await _sender.CreateMessageBatchAsync();
+
+        if (!messageBatch.TryAddMessage(new ServiceBusMessage(requestBody)))
+        {
+            _logger.LogError("Message too large for batch.");
+            return new BadRequestObjectResult("Message too large.");
+        }
+
+        try
+        {
+            await _sender.SendMessagesAsync(messageBatch);
+            _logger.LogInformation("Batch sent to topic.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to send batch.");
+            return new StatusCodeResult(500);
+        }
+
+        return new OkObjectResult("Payload received");
     }
+
 }
